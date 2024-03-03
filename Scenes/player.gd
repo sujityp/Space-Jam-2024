69d1e4020c257fdbee361dc64c66b2dcf64fad85
sujityp <sujityp@gmail.com>
2024-03-03 00:01:24 -0500

    player motion

---
+++ Scenes/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const FRIC = 0.4
+
+@onready var AnimTree = $AnimationTree
+@onready var AnimPlayer = $AnimationPlayer
+@onready var AnimState = AnimTree.get("parameters/playback")
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Vector2(Input.get_axis("ui_left", "ui_right"),Input.get_axis("ui_up", "ui_down"))
+	if direction.x and direction.y: direction.y = 0
+	
+	if direction:
+		AnimTree.set("parameters/Idle/blend_position", direction)
+		AnimTree.set("parameters/Run/blend_position", direction)
+		AnimState.travel("Run")
+		velocity = direction * SPEED
+	else:
+		AnimState.travel("Idle")
+		velocity = velocity.lerp(Vector2.ZERO, FRIC)
+
+	move_and_slide()
